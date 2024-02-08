e7df36f4cd8eb06798bdf80b9bccd568c05b047a
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-02-09 00:14:01 +0530

    Added put and delete in cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors({ 
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type']
 }));
 app.use("/api", router);
 
